2b0ccb6dc7687eba071d84cccf277693571d2ea5
Eggsaminator <bourouf1@hotmail.fr>
2022-10-03 19:44:51 -0400

    switch platform

--- Player.h
+++ Player.h
@@ -11,15 +11,16 @@ class Player {
 public:
 	Player();
 	Player(string name);
-	//vector<Territory*> toDefend();
-	//vector<Territory*> toAttack();
-	//void issueOrder(Orders newOrder);
+	vector<Territory*> toDefend();
+	vector<Territory*> toAttack();
+	void issueOrder(Orders newOrder);
 	vector<int*> getNums();
 	void setNum(int newVal);
 
 private:
 	string name;
-	//Hand* hand;
-	//vector<Territory*> territories;
+	Hand* hand;
+	vector<Territory*> territories;
 	vector<int*> nums;
+	OrderList* ordersList;
 };
--- PlayerDriver.cpp
+++ PlayerDriver.cpp
@@ -1,7 +1,12 @@
 #include "Player.h"
+#include <iostream>
+using namespace std;
 
 int main() {
 	Player p1 = Player("jose");
-
+	
+	cout << "now entering to defend method.\n";
+	p1.toDefend();
+	cout << "now exiting to defend method.\n";
 
 }
