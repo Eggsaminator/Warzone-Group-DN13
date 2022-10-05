bb36422de4904da6f4b19272ea49afff93677df7
Eggsaminator <bourouf1@hotmail.fr>
2022-10-05 11:08:30 -0400

    player implementations pre tests

--- Player.cpp
+++ Player.cpp
@@ -3,6 +3,7 @@
 #include "Map.h"
 #include "Orders.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 /*
@@ -23,7 +24,8 @@ private:
 */
 //dont know if we will need/keep this
 Player::Player() {
-
+name = "nameless player";
+territories.push_back(new Territory("TestTerritory of " + username, NULL));
 }
 
 //player constructor
@@ -32,24 +34,23 @@ Player::Player(string username) {
 	territories.push_back(new Territory("TestTerritory of " + username, NULL));
 }
 
-Player::issueOrder() {
-
+Player::issueOrder(string name) {
+Order orderToAdd = new Order(name);
+*ordersList.orders.push_back(&orderToAdd);
 }
 
 Player::toDefend() {
+	cout << "territories to defend : \n";
 	for (int i = 0; i < territories.size; i++) {
 		cout << "territory to defend number " << (i + 1) << " of player " << name << "\n";
 	}
-	cout << "those were all the territories to defend.\n"
+	cout << "those were all the territories to defend.\n";
 }
 
 Player::toAttack() {
-
-}
-Player::getNum() {
-
-}
-
-Player::setNum() {
-
+	vector<Territory> otherTerritories;
+	for (int i = 0; i < 10; i++) {
+		cout << "arbitraty territory to attack number " << (i + 1) << " of player " << Territory("Base #"  << i+1, NULL) << "\n";
+	}
+	cout << "those were all the territories to defend.\n";
 }
