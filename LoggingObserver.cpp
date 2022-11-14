d2e858d6b432a90377a0a8cf729d6b3ed2386ec7
Eggsaminator <61292159+Eggsaminator@users.noreply.github.com>
2022-11-13 23:36:55 -0500

    implemented test logger

--- LoggingObserver.cpp
+++ LoggingObserver.cpp
@@ -1,5 +1,8 @@
+#include <vector>
 #include "LoggingObesrver.h"
-
+#include "player.h"
+#include "GameEngine.h"
+#include "CommandProcessing.h"
 
 Observer::Observer(){
 }; 
@@ -43,5 +46,41 @@ void Subject::notify(ILoggable *toLog){
 
 
 void testLoggingObesrver(){
+  vector<string> mapList;
+	// initializing useful variables
+	mapList.push_back("Canada.map");
+	Player* sampleP = new Player("player");
+	Player* sampleP2 = new Player("not player");
+	Continent* continent = new Continent("test",0);
+	Territory* sampleTarget = new Territory("territory #1", continent);
+	Territory* sampleTarget1 = new Territory("territory #3", continent);
+	Territory* sampleSource = new Territory("territory #2", continent);
+
+
+	Orders* n1 = new Negotiate(sampleP, sampleP2);
+  //create order logger
+  LogObserver *n1Log = new LogObserver(n1);
+  //create orderlist logger
+  LogObserver *POlsLog = new LogObserver(sampleP->getOrdersList());
+  //test addOrder logging
+  sampleP->getOrdersList()->addOrder(n1);
+	//test execute logging
+	n1->execute();
+
+  Engine *eng = new Engine();
+  //create engine logger
+  LogObserver *engLog = new LogObserver(eng);
+  //test transition logging
+  eng->launchTransitionCommand("loadmap");
+
+  CommandProcessor *cmdP = new CommandProcessor(eng);
+  LogObserver *cmdPLog = new LogObserver(cmdP);
   
+  Command *cmd = new Command("logTesting Command");
+  LogObserver *cmdLog = new LogObserver(cmd);
+
+  cmd->saveEffect("test the logging");
+
+  cmdP->saveCommand(cmd);
+
 }
